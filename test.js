c265a63eff75d7447c849921f075cfd031422944
nikolaevra <Baller2000Dollar>
2017-12-21 22:39:54 +0300

    working

--- test.js
+++ test.js
@@ -1,9 +1,11 @@
 let api = require('./index.js');
 
+api.getUserLatestPosts('ev').then((data) => {
+    //console.log(data);
 
-api.getUserLatestPosts('nikolaevra', 1).then((data) => {
-    console.log(data);
 }).catch((err) => {
-    console.log(err);
+    console.log(`Test failed: ${err.message}`);
+    console.log(err.stack);
+    console.log("If you got \"Failed to load page, status code: 404\" error, make sure to check the user exists")
 });
 
